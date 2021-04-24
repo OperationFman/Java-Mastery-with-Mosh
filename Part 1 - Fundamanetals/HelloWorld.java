d86cd0b744c33fa808d0a1f1083d19958dc46835
Franklin Moon <franklin.m.moon@gmail.com>
2021-04-24 16:17:20 +1000

    Added date reference plus import

--- Part 1 - Fundamanetals/HelloWorld.java
+++ Part 1 - Fundamanetals/HelloWorld.java
@@ -1,14 +1,17 @@
+import java.util.Date;
+
 public class HelloWorld {
     public static void main(String[] args) {
         int age = 30;
         age = 35;
         int myAge = age;
+
         byte bitten = 30;
         long tooBig = 3999999999L;
         float price = 10.99F;
         char letter = 'A';
         boolean eligible = true;
-        
+
         System.out.println("Hello World");
         System.out.println(myAge);
         System.out.println(bitten);
@@ -16,5 +19,8 @@ public class HelloWorld {
         System.out.println(price);
         System.out.println(letter);
         System.out.println(eligible);
+
+        Date now = new Date();
+        System.out.println(now);
     }
 }
