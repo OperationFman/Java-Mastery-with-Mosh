3de703f684cdbdd2431f9e61d75cf7b05993ce1b
Franklin Moon <franklin.m.moon@gmail.com>
2021-04-24 16:12:41 +1000

    Learnt primitives

--- Part 1 - Fundamanetals/HelloWorld.java
+++ Part 1 - Fundamanetals/HelloWorld.java
@@ -1,5 +1,20 @@
 public class HelloWorld {
     public static void main(String[] args) {
+        int age = 30;
+        age = 35;
+        int myAge = age;
+        byte bitten = 30;
+        long tooBig = 3999999999L;
+        float price = 10.99F;
+        char letter = 'A';
+        boolean eligible = true;
+        
         System.out.println("Hello World");
+        System.out.println(myAge);
+        System.out.println(bitten);
+        System.out.println(tooBig);
+        System.out.println(price);
+        System.out.println(letter);
+        System.out.println(eligible);
     }
 }
