4fc52015cd3e3d26829158c42d2aec7d3ac82a05
Franklin Moon <franklin.m.moon@gmail.com>
2021-04-25 11:30:32 +1000

    Create MortgageCalculator.java

---
+++ Part 1 - Fundamanetals/MortgageCalculator.java
@@ -0,0 +1,9 @@
+public class MortgageCalculator {
+    public static void main(String[] args) {
+        // Ask for principal amount
+        // Ask for annual interest rate in percent, aka divide by 100
+        // Ask for period in years
+        // output mortage amount
+        
+    }
+}
