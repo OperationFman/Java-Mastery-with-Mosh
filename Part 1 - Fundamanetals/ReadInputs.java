3939e2f396f6c69619dda5466ed5897be16af870
Franklin Moon <franklin.m.moon@gmail.com>
2021-04-25 11:24:40 +1000

    Update ReadInputs.java

--- Part 1 - Fundamanetals/ReadInputs.java
+++ Part 1 - Fundamanetals/ReadInputs.java
@@ -3,8 +3,8 @@ import java.util.Scanner;
 public class ReadInputs {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter your age");
-        byte age = scanner.nextByte();
+        System.out.print("Enter your age: ");
+        byte age = scanner.nextByte(); // Just use .nextLine() for strings, if you dont you only get the first word (Called a token), line is what you're used to
         System.out.println("You are " + age);
     }
 }
