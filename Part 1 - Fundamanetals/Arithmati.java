b36278b2e37055e4373d218251d41e72649f92cf
Franklin Moon <franklin.m.moon@gmail.com>
2021-04-25 10:46:22 +1000

    Update Arithmati.java

--- Part 1 - Fundamanetals/Arithmati.java
+++ Part 1 - Fundamanetals/Arithmati.java
@@ -1,5 +1,6 @@
 public class arithmati {
     public static void main(String[] args) {
-        int result = (double)10 / (double)3;
+        int result = 10 / 3; // Returns 9, Java can only divide whole numbers.
+        float result2 = (double)10 / (double)3; // Returns 3.33. Add (double) to convert to float
     }
 }
