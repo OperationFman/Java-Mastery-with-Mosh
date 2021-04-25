5d2b61c2255710972d686ad80ee8b9314a9d55f4
Franklin Moon <franklin.m.moon@gmail.com>
2021-04-25 11:03:39 +1000

    Casting things

---
+++ Part 1 - Fundamanetals/Casts.java
@@ -0,0 +1,19 @@
+public class Casts {
+    public static void main(String[] args) {
+        // Casting is when a type is converted, in java this CAN be done automatically
+        // eg. byte > short > int > long > float > double
+        // since each gets bigger with each step. But can't be made smaller
+
+        double x = 1.1;
+        double y = x + 2;
+        System.out.println(y); // Returns 3.1 because int is automatically casted to a double. 1.1 can't be automatically casted to an int though
+
+        double a = 1.1;
+        int b = (int)a + 2; // Explicitly converting the type by prefixing (int) will remove the numbers after the decimal. aka data loss
+        System.out.println(b); // Returns 3
+
+        String j = "1";
+        int k = Integer.parseInt(j) + 2; // To convert string to an above data type, use .parse<datatype>(string)
+        System.out.println(k); // Returns 3
+    }
+}
