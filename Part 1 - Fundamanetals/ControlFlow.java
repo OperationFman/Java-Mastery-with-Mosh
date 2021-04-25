3e2428d699a9380881dfd9ce2cd1210a09e67a79
Franklin Moon <franklin.m.moon@gmail.com>
2021-04-25 14:42:14 +1000

    Update ControlFlow.java

--- Part 1 - Fundamanetals/ControlFlow.java
+++ Part 1 - Fundamanetals/ControlFlow.java
@@ -14,6 +14,7 @@ public class ControlFlow {
         System.out.println(isEligible2); // true
 
 
+
         int temp = 32;
         if (temp > 30) {
             System.out.println("It's a hot day");
@@ -22,6 +23,28 @@ public class ControlFlow {
             System.out.println("Beautiful Day");
         } else {
             System.out.println("It's a cold day");
+        }
+
+        int income = 120000;
+        boolean isHighIncome = income > 100000; // If this evaluates to true, it's true. Better than if else apparently
+        System.out.println(isHighIncome); // true
+
+        int earnings = 120000;
+        String className = earnings > 100000 ? "First Class" : "Economy"; // ternerary Operator, alternative to if else
+        System.out.println(className); // "First Class"
+        
+
+
+        String role = "admin";
+        switch (role) {
+            case "admin":
+                System.out.println("You're an admin"); // You're an admin
+                break; // Always add a break otherwise following stuff executes anyways for some reason
+            case "moderator":
+                System.out.println("You're a moderator");
+                break;
+            default: 
+                System.out.println("You are a guest");
         }
 
     }
