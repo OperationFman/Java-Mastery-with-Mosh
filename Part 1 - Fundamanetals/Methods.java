2c29a615b42bbab600c6147b03d10149037c43e1
Franklin Moon <franklin.m.moon@gmail.com>
2021-04-26 13:26:43 +1000

    Update Methods.java

--- Part 1 - Fundamanetals/Methods.java
+++ Part 1 - Fundamanetals/Methods.java
@@ -3,6 +3,8 @@ public class Methods {
     // static == 'A method that belongs to a class, instead of an object'
     // void == If not returning anything. Replace with 'int' if returning an int, etc
     // main() == Must always be at the start, similar to __init__()
+
+    
     public static void main(String[] args) {
         String message = greetUser("Franklin", "Moon");
         System.out.println(message);
