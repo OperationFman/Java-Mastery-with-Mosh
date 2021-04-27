97939c651a229c559c4a271ad3cb6ff6a30c3fe6
Franklin Moon <franklin.m.moon@gmail.com>
2021-04-27 21:06:27 +1000

    Create Main.java

---
+++ Part 2 - Object-Oriented/Classes/src/Main.java
@@ -0,0 +1,9 @@
+import classes.TextBox;
+
+public class Main {
+    public static void main(String[] args) {
+        var textBox1 = new TextBox();
+        textBox1.setText("Box 1");
+        System.out.println(textBox1.text);
+    }
+}
