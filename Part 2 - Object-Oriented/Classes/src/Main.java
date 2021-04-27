158ddffce4db895effb216c9b9a2cd42839c4d1f
Franklin Moon <franklin.m.moon@gmail.com>
2021-04-27 21:32:30 +1000

    Refactor out textbox

--- Part 2 - Object-Oriented/Classes/src/Main.java
+++ Part 2 - Object-Oriented/Classes/src/Main.java
@@ -1,20 +1,12 @@
-import classes.TextBox;
+import classes.Employee;
 
 public class Main {
     public static void main(String[] args) {
-        var textBox1 = new TextBox(); // New instance of textbox
-        textBox1.setText("Box 1");
-        System.out.println(textBox1.text); // Box 1
-
-        var textBox2 = new TextBox(); // New instance of textbox
-        textBox2.setText("Box 2");
-        System.out.println(textBox2.text); // Box 2
-
         int baseSalary = 50000;
         int extraHours = 10;
         int hourlyRate = 20;
         int wage = calculateWage(baseSalary, extraHours, hourlyRate);
-        System.out.println(wage);
+        System.out.println(wage); // 50200
         
     }
 
