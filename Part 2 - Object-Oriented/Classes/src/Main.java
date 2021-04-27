cda4c22b0b1e45fc77ee011d07f87586788927c2
Franklin Moon <franklin.m.moon@gmail.com>
2021-04-27 21:18:02 +1000

    Update Main.java

--- Part 2 - Object-Oriented/Classes/src/Main.java
+++ Part 2 - Object-Oriented/Classes/src/Main.java
@@ -2,8 +2,14 @@ import classes.TextBox;
 
 public class Main {
     public static void main(String[] args) {
-        var textBox1 = new TextBox();
+        var textBox1 = new TextBox(); // New instance of textbox
         textBox1.setText("Box 1");
-        System.out.println(textBox1.text);
+        System.out.println(textBox1.text); // Box 1
+
+        var textBox2 = new TextBox(); // New instance of textbox
+        textBox2.setText("Box 2");
+        System.out.println(textBox2.text); // Box 2
+
+        
     }
 }
