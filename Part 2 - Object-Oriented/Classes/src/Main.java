53caa40ad43c85bfd430b853b0a97f48a5759356
Franklin Moon <franklin.m.moon@gmail.com>
2021-04-27 21:46:05 +1000

    Setup addition getter and setter with comments

--- Part 2 - Object-Oriented/Classes/src/Main.java
+++ Part 2 - Object-Oriented/Classes/src/Main.java
@@ -3,10 +3,14 @@ import classes.Employee;
 public class Main {
     public static void main(String[] args) {
         var employee = new Employee();
-        employee.setBaseSalary(50000); // -1: Salary cannot be zero or less
-        employee.hourlyRate = 20;
-        int wage = employee.calculateWage(10);
+        employee.setBaseSalary(50000); // setter
+        employee.setHourlyRate(20); // setter
+        int wage = employee.calculateWage(10); // setter
+
+        int salary = employee.getBaseSalary(); // getter
+        int hourlyRate = employee.getHourlyRate(); // getter
         System.out.println(wage); // 50200
-        
+        System.out.println(salary); // 50000
+        System.out.println(hourlyRate); // 20
     }
 }
--- Part 2 - Object-Oriented/Classes/src/classes/Employee.java
+++ Part 2 - Object-Oriented/Classes/src/classes/Employee.java
@@ -2,7 +2,7 @@ package classes;
 
 public class Employee {
     private int baseSalary;
-    public int hourlyRate;
+    private int hourlyRate;
 
     public int calculateWage(int extraHours) {
         return baseSalary + extraHours * hourlyRate;
@@ -11,5 +11,18 @@ public class Employee {
     public void setBaseSalary(int baseSalary) {
         if (baseSalary <= 0) throw new IllegalArgumentException("Salary cannot be zero or less");
         this.baseSalary = baseSalary;
+    }
+
+    public int getBaseSalary() { // This is na getter/setter so class variables can;t be edited
+        return baseSalary;
+    }
+
+    public void setHourlyRate(int hourlyRate) {
+        if (hourlyRate <= 0) throw new IllegalArgumentException("Hourly Rate cannot be zero or less");
+        this.hourlyRate = hourlyRate;
+    }
+
+    public int getHourlyRate() { // This is na getter/setter so class variables can;t be edited
+        return hourlyRate;
     }
 }
