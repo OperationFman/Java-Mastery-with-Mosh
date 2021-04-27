ea5df5509765f16bf23d3620949e35284f021ee2
Franklin Moon <franklin.m.moon@gmail.com>
2021-04-27 21:41:40 +1000

    Refactored out error handling and exceptionaiting

--- Part 2 - Object-Oriented/Classes/src/Main.java
+++ Part 2 - Object-Oriented/Classes/src/Main.java
@@ -3,7 +3,7 @@ import classes.Employee;
 public class Main {
     public static void main(String[] args) {
         var employee = new Employee();
-        employee.baseSalary = 50000;
+        employee.setBaseSalary(50000); // -1: Salary cannot be zero or less
         employee.hourlyRate = 20;
         int wage = employee.calculateWage(10);
         System.out.println(wage); // 50200
--- Part 2 - Object-Oriented/Classes/src/classes/Employee.java
+++ Part 2 - Object-Oriented/Classes/src/classes/Employee.java
@@ -1,10 +1,15 @@
 package classes;
 
 public class Employee {
-    public int baseSalary;
+    private int baseSalary;
     public int hourlyRate;
 
     public int calculateWage(int extraHours) {
         return baseSalary + extraHours * hourlyRate;
+    }
+
+    public void setBaseSalary(int baseSalary) {
+        if (baseSalary <= 0) throw new IllegalArgumentException("Salary cannot be zero or less");
+        this.baseSalary = baseSalary;
     }
 }
