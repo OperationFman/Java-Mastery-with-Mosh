7343da1c20023c0aa5600e7714d758352e63a76d
Franklin Moon <franklin.m.moon@gmail.com>
2021-04-28 14:20:42 +1000

    Refactor to Reporting class

--- Part 2 - Object-Oriented/MortgageCalcOOP/src/MortgageCalc.java
+++ Part 2 - Object-Oriented/MortgageCalcOOP/src/MortgageCalc.java
@@ -1,9 +1,8 @@
-import java.text.NumberFormat;
-
 import calc.Console;
+import calc.Reporting;
 
 public class MortgageCalc {
-    final static byte MONTHS_IN_YEAR = 12;
+    public final static byte MONTHS_IN_YEAR = 12;
     final static byte PERCENT = 100;
 
     public static void main(String[] args) {
@@ -11,27 +10,8 @@ public class MortgageCalc {
         float annualInterest = (float) Console.readNumber("Annual Interest Rate: ", 1, 30);
         byte years = (byte) Console.readNumber("Period (Years): ", 1, 30);
 
-        printMortgage(principal, annualInterest, years);
-        printPaymentSchedule(principal, annualInterest, years);
-    }
-
-    private static void printMortgage(int principal, float annualInterest, byte years) {
-        double mortgage = calculateMortgage(principal, annualInterest, years);
-        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
-        System.out.println();
-        System.out.println("MORTGAGE");
-        System.out.println("--------");
-        System.out.println("Monthly Payments: " + mortgageFormatted);
-    }
-
-    private static void printPaymentSchedule(int principal, float annualInterest, byte years) {
-        System.out.println();
-        System.out.println("PAYMENT SCHEDULE");
-        System.out.println("----------------");
-        for (short month = 1; month <= years * MONTHS_IN_YEAR; month++) {
-            double balance = calculateBalance(principal, annualInterest, years, month);
-            System.out.println(NumberFormat.getCurrencyInstance().format(balance));
-        }
+        Reporting.printMortgage(principal, annualInterest, years);
+        Reporting.printPaymentSchedule(principal, annualInterest, years);
     }
 
     public static double calculateBalance(
