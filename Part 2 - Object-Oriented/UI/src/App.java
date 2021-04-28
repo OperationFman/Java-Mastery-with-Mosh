81be172125c4240568cc1ebd7abb54740609c524
Franklin Moon <franklin.m.moon@gmail.com>
2021-04-28 14:46:06 +1000

    Added UIcontrol

---
+++ Part 2 - Object-Oriented/UI/src/App.java
@@ -0,0 +1,9 @@
+package uiclasses;
+
+import uiclasses.UIControl;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
